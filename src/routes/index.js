1bc4411924e191ee7d61b64122cc02c00bda828b
Jovina J <jovinagustari17@gmail.com>
2024-10-17 22:07:51 +0700

    car_types

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const carsRouter = require("./cars");
+const carTypeRouter = require("./carType");
 
 const router = express.Router();
 
 router.use("/cars", carsRouter);
+router.use("/car-type", carTypeRouter);
 
 module.exports = router;
