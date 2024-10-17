257559a8f9555e60b40ee4c97899affea6fd482d
ikoindra <ikoindra91@gmail.com>
2024-10-18 03:06:57 +0700

    feature create models

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const carsRouter = require("./cars");
 const carsModelRouter = require("./carsModel");
 
 const router = express.Router();
 
-router.use("/cars", carsRouter);
-router.use("/models", carsModelRouter);
+router.use("/models", carsModelRouter); // /models -> carsModelRouter
 
 module.exports = router;
