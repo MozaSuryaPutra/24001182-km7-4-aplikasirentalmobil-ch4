3108030f9ddd432f0a179ce8116ce2011660b879
ikoindra <ikoindra91@gmail.com>
2024-10-18 18:04:32 +0700

    Feature create models (#16)
    * feature create model
    
    * feature create models
    
    * feature_create_models
    
    * feature_create_models
    
    * feature_create_models
    
    * feature_create_models

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const carsRouter = require("./cars");
-//const carTypeRouter = require("./carType");
+const carTypeRouter = require("./carType");
 const carsModelRouter = require("./carsModel");
 
 const router = express.Router();
